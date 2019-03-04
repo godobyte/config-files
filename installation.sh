e7f5d82acc70fa26ef7614279112d3d2f9484110
godobyte <godot.y.bian@gmail.com>
2019-03-04 00:02:48 -0800

    initial commit

---
+++ installation.sh
@@ -0,0 +1,10 @@
+# interactive shell fish
+sudo apt-get update
+sudo apt-get install fish
+
+# terminal multiplexer for Unix-like operating systems
+brew install tmux
+tmux -V
+
+# aws cli
+# https://docs.aws.amazon.com/cli/latest/userguide/install-macos.html
